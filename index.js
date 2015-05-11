5a576d190b977a2afee75714ee6b04ec7fb910c4
mbusenitz <mbusenitz@gmail.com>
2015-05-10 20:28:09 -0700

    pub

---
+++ index.js
@@ -0,0 +1,78 @@
+var assert = require( 'assert' )
+  , Connector = require( './connector.js' )
+  , cp = require( 'child_process' );
+
+assert( typeof Connector === 'function' );
+
+function mule(pack, options, done) {
+
+  var connector;
+  assert( Array.isArray(pack) && pack.length > 0 );
+  
+  if (typeof options === 'undefined') {
+    options = {};
+  }
+  
+  init(options, function() {
+    connector = new Connector( options );
+    processCommand();
+  });
+
+  function processCommand() {
+    
+    var command;
+    assert(pack.length);
+    
+    command = pack[0][0];
+    args = pack[0].slice(1);
+    pack.splice(0,1);
+
+    if (pack.length) {
+      connector.pipeIn(function(context) {
+        spawn( command, args, context );
+        processCommand();
+      });
+    }
+    else if (connector.isActive()) {
+      connector.pipeOut(function(context) {
+        done( spawn( command, args, context ) );
+      });
+    }
+    else {
+      done( spawn( command, args, options ) );
+    }
+  }
+
+  function spawn(command, args, context) {
+    var opt = {};
+    for(var i in options) {
+      if (i == 'stdio') {
+        opt.stdio = [context.stdin, context.stdout, context.stderr];
+      }
+      else {
+        opt[i] = options[i];
+      }
+    }
+    
+    return cp.spawn( command, args, opt );
+  }
+
+  function init(options, cb) {
+
+    if (!options.hasOwnProperty('cwd')) {
+      options.cwd = process.cwd();
+    }
+
+    ['stdin', 'stdout', 'stderr']
+    .forEach(function(name, index, names) {
+      if (!options.hasOwnProperty(name)) {
+        options[name] = process[name];
+      }
+      if (index == names.length - 1) {
+        cb();
+      }
+    });
+  }
+}
+
+module.exports = mule;
---
+++ test/test.js
@@ -0,0 +1,134 @@
+var assert = require( 'assert' )
+  , Expector = require( 'expector' ).SeqExpector
+  , path = require( 'path' )
+  , mule = require( '../index.js' );
+
+assert( typeof mule === 'function' );
+assert( typeof Expector === 'function' );
+
+suite( 'mule', function() {
+
+  var expector;
+
+  setup(function() {
+    expector = new Expector();
+  });
+
+  teardown(function() {
+    process.nextTick( expector.check );
+  });
+
+  test( 'stdout option with single pipe', function() {
+    mule( [['ls']], { stdout: 'pipe' }, function(child) {
+      assert( typeof child !== 'undefined' );
+      assert( child.hasOwnProperty( 'stdout' ) ); 
+    });
+  });
+
+  test( 'stdout option with multiple pipe', function() {
+    mule( [['ls'], ['less']], { stdout: 'pipe' }, function(child) {
+      assert( typeof child !== 'undefined' );
+      assert( child.hasOwnProperty( 'stdout' ) ); 
+    } );
+  });
+
+  test( 'cwd option', function(done) {
+    var options = { 
+          cwd: path.join( __dirname, 'sample' ),
+          stdout: 'pipe'
+    };
+
+    mule( [['ls']], options, function(child) {
+      var result = '';
+      child.stdout.on( 'data', function(data) {
+        result += data.toString();
+      });
+
+      child.on('close', function() {
+        assert( /test\.txt/.test( result ) );
+        done();
+      });
+    } );
+  });
+
+  test( 'check stderr', function(done) {
+    expector.expectNot( 'stdout' );
+    expector.expect( 'stderr' );
+    
+    mule( 
+      [['cat', 'doesNotExist.txt']], 
+      {
+        controller: expector,
+        stdout: 'pipe',
+        stdin: 'pipe',
+        stderr: 'pipe'
+      }, 
+      function(child) {
+      
+        child.stderr.on( 'data', function(data) {
+          expector.emit( 'stderr' );
+        });
+
+        child.stdout.on( 'data', function(data) {
+          expector.emit( 'stdout' );
+        });
+
+        child.on( 'close', function() {
+          process.nextTick( done );
+        });
+
+      });
+  });
+
+  test( 'check stdout', function(done) {
+
+    expector.expectNot( 'stderr' );
+    expector.expect( 'stdout' );
+
+    mule( 
+      [['ls']], 
+      {
+        controller: expector,
+        stdout: 'pipe',
+        stdin: 'pipe',
+        stderr: 'pipe'
+      },
+      function(child) {
+        child.stderr.on( 'data', function(data) {
+          expector.emit( 'stderr' );
+        });
+
+        child.stdout.on( 'data', function(data) {
+          expector.emit( 'stdout' );
+        });
+
+        child.on( 'close', function() {
+          process.nextTick( done );
+        });
+      });
+  });
+
+  test( 'check stdin', function(done) {
+    var options = {
+          controller: expector,
+          stdout: 'pipe',
+          stdin: 'pipe',
+          env: process.env
+        };
+    options.env.PATH += ':/Users/markymark/work/mulepack/test/bin';
+    expector.expect( 'exit' );
+    mule( 
+      [['dummy_read']],
+      options, 
+      function(child) {
+        child.stdin.write('a\n');
+        child.on( 'close', function() {
+          process.nextTick( done );
+        });
+        child.on( 'exit', function() {
+          expector.emit( 'exit' );
+        });
+      });
+  });
+
+});
