90f070daddd3c87027d26ab1191c70c932e3d7c5
mbusenitz <mbusenitz@gmail.com>
2016-04-02 03:05:24 -0700

    cleanup

--- test/color.js
+++ test/color.js
@@ -1,16 +1,3 @@
 #!/usr/bin/env node
 
-'use strict';
-
-/*
-let ansi = require( 'ansi' )
-  , cursor = ansi(process.stdout);
-
-cursor
-	.red()
-	.write('hello')
-	.reset();
-
-	*/ 
-
-	console.log( '\x1b[31mhello\x1b[39;49m' ); 
+console.log( '\x1b[31mhello\x1b[39;49m' ); 
