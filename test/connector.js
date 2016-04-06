fbead5d02237e058d6b833c834b0fb124e4fdde3
mbusenitz <mbusenitz@gmail.com>
2016-04-06 05:01:16 -0700

    test pipeout

--- test/connector.js
+++ test/connector.js
@@ -7,21 +7,46 @@ let test = require( 'tape' )
   , Connector = require( '../connector.js' )
   , fs = require( 'fs' );
 
+test( 'connector pipeOut', (t) => {
+  var c = new Connector( { stdout: '', stdin: '', stderr: '' })
+    , buffer = new Buffer("hello");
+
+  c
+  .pipeIn()
+  .then( result => {
+    fs.write( result.stdout, buffer.toString(), (err) => {
+      if (err) throw err;
+      c
+      .pipeOut()
+      .then( result => {
+        let tmp = new Buffer(buffer.length);
+        fs.read( result.stdin, tmp, 0, tmp.length, 0, (err, bytesRead, bufferRead) => {
+          if (err) throw err;
+          t.assert( bufferRead.toString().trim() === buffer.toString().trim() );
+          t.end(); 
+        });
+      });
+    } ); 
+    
+  });
+
+});
+
 test( 'connector pipeIn', (t) => {
-	var c = new Connector( { stdout: '', stdin: '', stderr: '' }); 
-	c
-	.pipeIn()
-	.then( ( result ) => {
-		
-		t.assert( result.hasOwnProperty( 'stdout' ) );
-		t.assert( c.isActive() );
-		fs.write( result.stdout, "hello", (err) => {
-			t.assert(!err);
-			t.end(); 
-		} ); 
-	})
-	.catch( () => {
-		t.fail();
-		t.end();
-	});
+  var c = new Connector( { stdout: '', stdin: '', stderr: '' }); 
+  c
+  .pipeIn()
+  .then( ( result ) => {
+    
+    t.assert( result.hasOwnProperty( 'stdout' ) );
+    t.assert( c.isActive() );
+    fs.write( result.stdout, "hello", (err) => {
+      t.assert(!err);
+      t.end(); 
+    } ); 
+  })
+  .catch( () => {
+    t.fail();
+    t.end();
+  });
 });
